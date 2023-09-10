8b796100bddeb6a4014e9ddf3c245159a4959e21
jaeseokshin5245 <jaeseokshin5245@gmail.com>
2023-09-10 21:45:44 +0900

    Init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is a web page of The Krusty Krab
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
