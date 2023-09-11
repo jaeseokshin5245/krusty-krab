7cf25d0bf10e8f161821784f020a81a945c55c8c
jaeseokshin5245 <jaeseokshin5245@gmail.com>
2023-09-11 23:49:32 +0900

    head, logo clear

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
-import { BrowserView, MobileView } from 'react-device-detect'
+import Title from './Title.png'
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserView>
-        데스크톱브라우져!
-      </BrowserView>
-      <MobileView>
-        모바일 브라우져!
-      </MobileView>
-      
+      <header>
+        <div>
+          <img src={Title}/>
+          <h1 className='header-button'>KRAB KRUSTY</h1>
+          <h1 className='header-button'>KRAB MENU</h1>
+          <h1 className='header-button'>KRAB HISTORY</h1>
+          <h1 className='header-button'>KRAB FAMILY</h1>
+        </div>
+      </header>
+      <body>
+        <p>THIS IS BODY</p>
+      </body>
+
     </div>
   );
 }
